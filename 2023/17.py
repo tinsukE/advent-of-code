190baa84056b2bbb789a4beb96c22426fe23ed66
Angelo Suzuki <1063155+tinsukE@users.noreply.github.com>
2023-12-20 13:27:30 +0100

    Solve day 20 part 1

--- 2023/17.py
+++ 2023/17.py
@@ -1,63 +1,67 @@
 # https://adventofcode.com/2023/day/17
 
-def distance_heuristic(grid, point, end):
-	return abs(point[0] - end[0]) * abs(point[1] - end[1])
-
-def reconstruct_path(came_from, current):
-	total_path = [current]
-	while current in came_from:
-		current = came_from[current]
-		total_path.insert(0, current)
-	return total_path
-
 def calculate_neighbors(grid, point):
-	neighbors = []
+	neighbors = {}
 	if point[0] > 0:
-		neighbors.append((point[0] - 1, point[1]))
+		neighbors['U'] = (point[0] - 1, point[1])
 	if point[0] < len(grid) - 1:
-		neighbors.append((point[0] + 1, point[1]))
+		neighbors['D'] = (point[0] + 1, point[1])
 	if point[1] > 0:
-		neighbors.append((point[0], point[1] - 1))
+		neighbors['L'] = (point[0], point[1] - 1)
 	if point[1] < len(grid[0]) - 1:
-		neighbors.append((point[0], point[1] + 1))
+		neighbors['R'] = (point[0], point[1] + 1)
 	return neighbors
 
 def position_diff(a, b):
 	return (a[0] - b[0], a[1] - b[1])
 
-def a_star(grid, start, end):
+def my_star(grid, start, end):
+	INFINITY = float('inf')
+
 	open_set = {start}
 	came_from = {}
 
-	g_score = {}
-	g_score[start] = 0
-
-	f_score = {}
-	f_score[start] = 0 #distance_heuristic(grid, start, end)
+	cost_map = {}
+	cost_map[start] = {'U': 0, 'D': 0, 'L': 0, 'R': 0, 'C': 0}
 
 	while open_set:
-		current = min(open_set, key = lambda pos: f_score.get(pos, 999999999))
+		current = min(open_set, key=lambda pos: cost_map[pos]['C'])
+		print('current', current, 'cost', cost_map[current])
 		if current == end:
 			print('found!')
-			print(g_score)
-			return reconstruct_path(came_from, current)
+			return cost_map[current]['C']
 
 		open_set.remove(current)
-		for neighbor in calculate_neighbors(grid, current):
-			direction = position_diff(neighbor, current)
-			# previous = came_from.get(current)
-			# if previous and direction == position_diff(current, previous):
-			# 	pre_previous = came_from.get(previous)
-			# 	if pre_previous and direction == position_diff(previous, pre_previous):
-			# 		continue
+		for direction, neighbor in calculate_neighbors(grid, current).items():
+			current_movement = cost_map[current][direction]
+			current_cost = cost_map[current]['C']
+			if current_movement == 3:
+				continue
 
-			tentative_gscore = g_score[current] + grid[neighbor[0]][neighbor[1]]
-			if tentative_gscore < g_score.get(neighbor, 999999999):
-				came_from[neighbor] = current
-				g_score[neighbor] = tentative_gscore
-				f_score[neighbor] = tentative_gscore # + distance_heuristic(grid, neighbor, end)
-				if neighbor not in open_set:
-					open_set.add(neighbor)
+			print('evaluating neighbor', neighbor)
+			neighbor_cost = current_cost + grid[neighbor[0]][neighbor[1]]
+			if neighbor not in cost_map:
+				print('first neighbor!')
+				cost_map[neighbor] = {'U': 0, 'D': 0, 'L': 0, 'R': 0, 'C': neighbor_cost}
+				cost_map[neighbor][direction] = current_movement + 1
+			elif neighbor_cost < cost_map[neighbor]['C']:
+				print('best neighbor!')
+				cost_map[neighbor] = {'U': 0, 'D': 0, 'L': 0, 'R': 0, 'C': neighbor_cost}
+				cost_map[neighbor][direction] = current_movement + 1
+			elif neighbor_cost == cost_map[neighbor]['C']:
+				print('existing neighbor!')
+				if any(key != 'C' and key != direction and cost_map[neighbor][key] > 0 for key in cost_map[neighbor]):
+					cost_map[neighbor] = {'U': 0, 'D': 0, 'L': 0, 'R': 0, 'C': neighbor_cost}
+				elif cost_map[neighbor][direction] < current_movement + 1:
+					cost_map[neighbor][direction] = current_movement + 1
+				# else:
+				# 	print('bad existing neighbor...')
+				# 	continue
+			else:
+				print('bad neighbor... cost', neighbor_cost, 'previously', cost_map[neighbor])
+				continue
+			if neighbor not in open_set:
+				open_set.add(neighbor)
 
 	print('oops...')
 	return None
@@ -67,9 +71,7 @@ def solve_1(filename):
 	grid = [[int(char) for char in line.strip()] for line in file.readlines()]
 	for row in grid: print(row)
 
-	path = a_star(grid, (0, 0), (len(grid) - 1, len(grid[0]) - 1))
-	print(path)
-	heat_loss = sum([grid[pos[0]][pos[1]] for pos in path[1:]])
+	heat_loss = my_star(grid, (0, 0), (len(grid) - 1, len(grid[0]) - 1))
 	print(filename, 'heat_loss', heat_loss)
 
 solve_1('17_sample.txt')
