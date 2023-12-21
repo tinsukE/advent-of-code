95071405ee3ac9bce6e2b9f66a1d2f4f65615328
Angelo Suzuki <1063155+tinsukE@users.noreply.github.com>
2023-12-21 08:47:36 +0100

    Day 17 WIP

--- 2023/17.py
+++ 2023/17.py
@@ -12,8 +12,44 @@ def calculate_neighbors(grid, point):
 		neighbors['R'] = (point[0], point[1] + 1)
 	return neighbors
 
-def position_diff(a, b):
-	return (a[0] - b[0], a[1] - b[1])
+def build_initial_cost(cost):
+	return {'U': {0: cost}, 'D': {0: cost}, 'L': {0: cost}, 'R': {0: cost}}
+
+def add_cost(cost_map, pos, direction, movement, cost):
+	added = False
+	for ite_direction, ite_costs in cost_map[pos].items():
+		if ite_direction == direction:
+			skip = False
+			for ite_movement in range(0, movement):
+				if ite_movement in ite_costs and cost >= ite_costs[ite_movement]:
+					skip = True
+					break
+			if skip:
+				continue
+			if movement not in ite_costs or cost < ite_costs[movement]:
+				ite_costs[movement] = cost
+				added = True
+			for ite_movement in range(movement + 1, 4):
+				if ite_movement in ite_costs and cost <= ite_costs[ite_movement]:
+					ite_costs.pop(ite_movement)
+					added = True
+		else:
+			if 0 not in ite_costs or cost < ite_costs[0]:
+				ite_costs[0] = cost
+				added = True
+			for ite_movement, ite_cost in dict(ite_costs).items():
+				if cost < ite_cost:
+					ite_costs.pop(ite_movement)
+					added = True
+	return added
+
+def calculate_min_cost(costs):
+	min_cost = None
+	for direction_costs in costs.values():
+		for cost in direction_costs.values():
+			if min_cost == None or cost < min_cost:
+					min_cost = cost
+	return min_cost
 
 def my_star(grid, start, end):
 	INFINITY = float('inf')
@@ -22,46 +58,35 @@ def my_star(grid, start, end):
 	came_from = {}
 
 	cost_map = {}
-	cost_map[start] = {'U': 0, 'D': 0, 'L': 0, 'R': 0, 'C': 0}
+	cost_map[start] = build_initial_cost(0)
 
 	while open_set:
-		current = min(open_set, key=lambda pos: cost_map[pos]['C'])
+		current = min(open_set, key=lambda pos: calculate_min_cost(cost_map[pos]))
 		print('current', current, 'cost', cost_map[current])
 		if current == end:
-			print('found!')
-			return cost_map[current]['C']
+			return calculate_min_cost(cost_map[current])
 
 		open_set.remove(current)
 		for direction, neighbor in calculate_neighbors(grid, current).items():
-			current_movement = cost_map[current][direction]
-			current_cost = cost_map[current]['C']
-			if current_movement == 3:
-				continue
+			current_direction_costs = cost_map[current][direction]
+			for current_movement, current_cost in current_direction_costs.items():
+				if current_movement == 3:
+					continue
 
-			print('evaluating neighbor', neighbor)
-			neighbor_cost = current_cost + grid[neighbor[0]][neighbor[1]]
-			if neighbor not in cost_map:
-				print('first neighbor!')
-				cost_map[neighbor] = {'U': 0, 'D': 0, 'L': 0, 'R': 0, 'C': neighbor_cost}
-				cost_map[neighbor][direction] = current_movement + 1
-			elif neighbor_cost < cost_map[neighbor]['C']:
-				print('best neighbor!')
-				cost_map[neighbor] = {'U': 0, 'D': 0, 'L': 0, 'R': 0, 'C': neighbor_cost}
-				cost_map[neighbor][direction] = current_movement + 1
-			elif neighbor_cost == cost_map[neighbor]['C']:
-				print('existing neighbor!')
-				if any(key != 'C' and key != direction and cost_map[neighbor][key] > 0 for key in cost_map[neighbor]):
-					cost_map[neighbor] = {'U': 0, 'D': 0, 'L': 0, 'R': 0, 'C': neighbor_cost}
-				elif cost_map[neighbor][direction] < current_movement + 1:
-					cost_map[neighbor][direction] = current_movement + 1
-				# else:
-				# 	print('bad existing neighbor...')
-				# 	continue
-			else:
-				print('bad neighbor... cost', neighbor_cost, 'previously', cost_map[neighbor])
-				continue
-			if neighbor not in open_set:
-				open_set.add(neighbor)
+				neighbor_cost = current_cost + grid[neighbor[0]][neighbor[1]]
+				print('evaluating neighbor', neighbor, direction, current_movement + 1, neighbor_cost, 'previously', cost_map.get(neighbor))
+				if neighbor not in cost_map:
+					print('first neighbor!')
+					cost_map[neighbor] = build_initial_cost(neighbor_cost)
+					cost_map[neighbor][direction] = {current_movement + 1: neighbor_cost}
+				elif add_cost(cost_map, neighbor, direction, current_movement + 1, neighbor_cost):
+					print('updated neighbor!')
+					pass
+				else:
+					print('bad neighbor...')
+					continue
+				if neighbor not in open_set:
+					open_set.add(neighbor)
 
 	print('oops...')
 	return None
@@ -75,3 +100,6 @@ def solve_1(filename):
 	print(filename, 'heat_loss', heat_loss)
 
 solve_1('17_sample.txt')
+# solve_1('17_input.txt')
+
+# 680 < answer < 701
