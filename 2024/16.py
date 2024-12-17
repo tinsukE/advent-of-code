245ae6b1a2335d6b0173dd96a2de2bca773397b9
Angelo Suzuki <1063155+tinsukE@users.noreply.github.com>
2024-12-17 13:15:57 +0100

    2024: solve day 16p2

--- 2024/16.py
+++ 2024/16.py
@@ -27,40 +27,65 @@ MOVES = {'<': (0, -1), '>': (0, 1), '^': (-1, 0), 'v': (1, 0)}
 
 def solve(filename):
 	area, start, end = parse_input(filename)
-	print_area(area)
+	# print_area(area)
 	print('start:', start, '| end:', end)
 
 	node = (start, '>')
-	node_costs = { node: 0 }
+	node_costs = { node: (0, set([start])) }
 	nodes = []
-	while node[0] != end:
+	while node is not None:
 		pos = node[0]
 		direction = node[1]
 		move = MOVES[direction]
-		cost = node_costs[node]
+		cost, tiles = node_costs[node]
 		for new_direction, new_move in MOVES.items():
 			new_pos = (pos[0] + new_move[0], pos[1] + new_move[1])
 			if not is_within_bounds(area, new_pos) or area[new_pos[0]][new_pos[1]] == '#':
 				continue
 
 			new_cost = None
+			# forward
 			if move == new_move:
 				new_cost = cost + 1
+			# 180 turn + forward
 			elif move[0] == new_move[0] or move[1] == new_move[1]:
 				new_cost = cost + 2000 + 1
+			# 90 turn + forward
 			else:
 				new_cost = cost + 1000 + 1
 
 			new_node = (new_pos, new_direction)
 			current_cost = node_costs.get(new_node)
-			if current_cost is None or new_cost < current_cost:
-				node_costs[new_node] = new_cost
+			if current_cost is not None and 1 in current_cost[1]:
+				print(current_cost)
+				return
+			new_tiles = set(tiles)
+			new_tiles.add(new_pos)
+			if current_cost is None or new_cost < current_cost[0]:
+				node_costs[new_node] = (new_cost, new_tiles)
 
 				if new_node in nodes: nodes.remove(new_node)
 				bisect.insort(nodes, new_node, key=lambda n: node_costs[n])
-		node = nodes.pop(0)
-		
-	print(node, node_costs[node])
+			elif new_cost == current_cost[0]:
+				new_tiles.update(current_cost[1])
+				node_costs[new_node] = (new_cost, new_tiles)
+		node = nodes.pop(0) if len(nodes) > 0 else None
+
+	lowest_cost = None
+	visited_tiles = set()
+	for direction in MOVES.keys():
+		rnode = (end, direction)
+		cost = node_costs.get(rnode)
+		if cost is None:
+			continue
+		elif lowest_cost is None or cost[0] < lowest_cost:
+			lowest_cost = cost[0]
+			visited_tiles = set(cost[1])
+		elif cost[0] == lowest_cost:
+			visited_tiles.append(cost[1])
+
+	print('lowest_cost', lowest_cost)
+	print('visited_tiles:', len(visited_tiles))
 
 solve('16_sample1.txt') # 7036
 solve('16_sample2.txt') # 11048
