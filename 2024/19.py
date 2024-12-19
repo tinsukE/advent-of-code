ee45ff483094fc82ed0b01c11579c541ae150390
Angelo Suzuki <1063155+tinsukE@users.noreply.github.com>
2024-12-19 19:38:19 +0100

    2024: solve day 19

---
+++ 2024/19.py
@@ -0,0 +1,36 @@
+# https://adventofcode.com/2024/day/18
+
+from functools import cache
+
+def parse_input(filename):
+	file = open(filename, 'r')
+	lines = file.readlines()
+	return lines[0].strip().split(', '), [l.strip() for l in lines[2:]]
+
+def is_possible(towels, pattern):
+	for towel in towels:
+		if towel == pattern:
+			return True
+		if pattern.startswith(towel) and is_possible(towels, pattern[len(towel):]):
+			return True
+	return False
+
+@cache
+def count_possible(towels, pattern):
+	count = 0
+	for towel in towels:
+		if towel == pattern:
+			count += 1
+		elif pattern.startswith(towel):
+			count += count_possible(towels, pattern[len(towel):])
+	return count
+
+def solve(filename):
+	towels, patterns = parse_input(filename)
+	possible = sum([is_possible(towels, pattern) for pattern in patterns])
+	print('possible', possible)
+	possible = sum([count_possible(tuple(towels), pattern) for pattern in patterns])
+	print('count_possible', possible)
+
+solve('19_sample.txt') # 6, 16
+solve('19_input.txt') # 213, 1016700771200474
